316f2077003eb78cb88be003c27d9463640019ce
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-18 16:04:50 +0600

    add firebase configuration

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNGP232rTMCsdGr9VuCGMBym1cbPqzeKg",
+  authDomain: "fir-auth-recap-26e18.firebaseapp.com",
+  projectId: "fir-auth-recap-26e18",
+  storageBucket: "fir-auth-recap-26e18.appspot.com",
+  messagingSenderId: "970654708719",
+  appId: "1:970654708719:web:92a35d775fda039ff17c1a"
+};
+
+export default firebaseConfig;
